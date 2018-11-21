387f4c8765f9084e9110e517adf58caaec1add0f
ChrisWOle <chriswole@gmail.com>
2018-11-21 13:36:05 +0100

    Added Location (not done) and Path

---
+++ Path of Destiny - ITTAPK Projekt E18/Code/Location.hpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+class Location
+{
+private:
+    
+public:
+    Location(string name);
+    ~Location();
+};
+
+Location::Location(string name)
+{
+
+}
+
+Location::~Location()
+{
+}
