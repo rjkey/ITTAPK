348161b415dd8c2cf50688a44b3d790ee63565e4
ChrisWOle <chriswole@gmail.com>
2018-12-31 15:22:19 +0100

    made a welcome to the game and to enter a heroname then initializee the game and starts the game.

--- Path of Destiny - ITTAPK Projekt E18/Code/Main.cpp
+++ Path of Destiny - ITTAPK Projekt E18/Code/Main.cpp
@@ -1,25 +1,25 @@
 
 
 #include <time.h>
-#include "Location.hpp"
+//#include "Location.hpp"
 #include "PathOfDestiny.hpp"
 //#include "Arena.hpp"
 //#include "Path.hpp"
 
 
 int main()
-{
-   
-
-  
+{ 
+    std::string name;
     // Init rand to be more random. 
     srand (time(NULL));
 
+    std::cout << "Welcome to Path of Destiny\nName your hero: ";
+    Hero hero( name, (HP)200, (ATT)15, (DEF)15);
     PathOfDestiny game = PathOfDestiny();
-    cout << "Game start: \n"; 
+    std::cout << "Game start: \n"; 
 
 
-    for(size_t i = 0; i < 10; i++)
+    for(size_t i = 0; i < 3; i++)
     {
         //game.movement();
         game.gameLogic();
